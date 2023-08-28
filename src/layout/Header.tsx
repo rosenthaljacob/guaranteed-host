728563149f7f08153937d9bb1e1da5d758ebb977
R15NTL <120052967+R15NTL@users.noreply.github.com>
2023-08-28 16:50:23 +0100

    Header styled

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Link from "next/link";
 // Hooks
 import { useScrollPosition } from "@/hooks/useScrollPosition";
 // Utils
 import { cn } from "@/lib/utils";
+// Components
+import { Button } from "@/components/ui/button";
 
 export default function Header() {
   const scrollPosition = useScrollPosition();
@@ -12,16 +15,35 @@ export default function Header() {
   return (
     <header
       className={cn(
-        "h-20 fixed w-full top-0 flex items-center px-5 z-50",
+        "h-20 fixed w-full top-0 flex justify-between items-center px-5 z-50",
         isOffset && "bg-background/50 backdrop-blur-lg shadow-md"
       )}
       style={{
         transition: "all 0.4s ease-in-out",
       }}
     >
-      <div className="max-w-7xl px-5 w-full mx-auto font-extrabold text-xl">
-        <p className="font-bold text-2xl tracking-wide">GuaranteedHost</p>
-      </div>
+      <Link
+        href="/"
+        className="max-w-7xl px-5 w-full mx-auto font-extrabold text-xl flex gap-1.5 items-center text-cyan-950"
+      >
+        <img
+          src="/logo.svg"
+          alt="GaranteedHost"
+          className="w-5 h-5 sm:w-8 sm:h-8"
+        />
+        <p
+          className="text-xl sm:text-2xl tracking-wide font-medium"
+          style={{
+            fontFamily: "Barlow Condensed, monospace",
+          }}
+        >
+          GUARANTEEDHOST
+        </p>
+      </Link>
+      <nav className="flex gap-3 items-center">
+        <Button variant="link">Login</Button>
+        <Button className="whitespace-nowrap">Sign Up</Button>
+      </nav>
     </header>
   );
 }
