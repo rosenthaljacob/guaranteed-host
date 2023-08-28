02af7ea4b20ebaaa252962209beb2a35c99ab639
R15NTL <120052967+R15NTL@users.noreply.github.com>
2023-08-28 16:59:57 +0100

    Favicons updated and linked

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,9 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@500&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/favicon/favicon32x32.png" sizes="32x32" />
+        <link rel="icon" href="/favicon/favicon128x128.png" sizes="128x128" />
+        <link rel="icon" href="/favicon/favicon512x512.png" sizes="512x512" />
       </Head>
       <body>
         <Main />
