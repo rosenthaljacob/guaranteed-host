2e70c98c44de89e72db915aad1c3bc63ff1a9fff
R15NTL <120052967+R15NTL@users.noreply.github.com>
2023-08-28 18:52:16 +0100

    Header layout fixed

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -22,28 +22,30 @@ export default function Header() {
         transition: "all 0.4s ease-in-out",
       }}
     >
-      <Link
-        href="/"
-        className="max-w-7xl w-full mx-auto font-extrabold text-xl flex gap-1.5 items-center text-cyan-950 pl-1"
-      >
-        <img
-          src="/logo.svg"
-          alt="GaranteedHost"
-          className="w-5 h-5 sm:w-8 sm:h-8"
-        />
-        <p
-          className="text-xl sm:text-2xl tracking-wide font-medium"
-          style={{
-            fontFamily: "Barlow Condensed, monospace",
-          }}
+      <div className="max-w-7xl w-full mx-auto flex gap-3 items-center px-5">
+        <Link
+          href="/"
+          className="font-extrabold text-xl flex gap-1.5 items-center text-cyan-950 pl-1"
         >
-          GUARANTEEDHOST
-        </p>
-      </Link>
-      <nav className="flex gap-3 items-center">
-        <Button variant="link">Login</Button>
-        <Button className="whitespace-nowrap">Sign Up</Button>
-      </nav>
+          <img
+            src="/logo.svg"
+            alt="GaranteedHost"
+            className="w-5 h-5 sm:w-8 sm:h-8"
+          />
+          <p
+            className="text-xl sm:text-2xl tracking-wide font-medium"
+            style={{
+              fontFamily: "Barlow Condensed, monospace",
+            }}
+          >
+            GUARANTEEDHOST
+          </p>
+        </Link>
+        <nav className="flex gap-3 ml-auto items-center">
+          <Button variant="link">Login</Button>
+          <Button className="whitespace-nowrap">Sign Up</Button>
+        </nav>
+      </div>
     </header>
   );
 }
